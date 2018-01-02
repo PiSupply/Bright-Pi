03b1089fd5047448bf8c74ba15c688ef5bf2e7a1
Garage Tech <francesco.vannini@happylot.net>
2018-01-02 11:17:56 +0000

    Fixed typo
    Fixed typo

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Enable I2C
 raspi-config nonint do_i2c 0
 
-git clone --depth=1 https://github.com/PiSupply/BrightPi.git
+git clone https://github.com/PiSupply/Bright-Pi.git
 
 whiptail --msgbox "The system will now reboot" 8 40
 reboot
